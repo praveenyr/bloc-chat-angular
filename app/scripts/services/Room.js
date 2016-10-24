537850f807b30dafa3d1f2ea80a1665b5d2f1b7a
Praveen Yallakara <praveenyr@gmail.com>
2016-10-24 09:47:34 -0700

    Complete Create-Chat-Rooms

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,17 +1,26 @@
 (function() {
   function Room($firebaseArray) {
+
     var ref = firebase.database().ref().child("rooms");
-
-
-
     var rooms = $firebaseArray(ref);
 
     window.foo = rooms;
 
+    var addRoom = function(name){
+      rooms.$add({ name: name });
+    };
+
+    addRoom("Code");
+    addRoom("Sports");
+    addRoom("Hangout");
+
     return {
       all: rooms
     };
+
   }
+
+
 
   angular
     .module('blocChat')
