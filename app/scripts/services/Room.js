bf56055762b0467bb0b289d1aeec9b6209c37559
Praveen Yallakara <praveenyr@gmail.com>
2016-10-24 11:15:13 -0700

    Cleaned up dist

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,26 +1,19 @@
 (function() {
   function Room($firebaseArray) {
-
     var ref = firebase.database().ref().child("rooms");
+
     var rooms = $firebaseArray(ref);
 
     window.foo = rooms;
 
-    var addRoom = function(name){
-      rooms.$add({ name: name });
-    };
-
-    addRoom("Code");
-    addRoom("Sports");
-    addRoom("Hangout");
+    rooms.$add({name:"Code"});
+    rooms.$add({name:"Sports"});
+    rooms.$add({name:"Hangout"});
 
     return {
       all: rooms
     };
-
   }
-
-
 
   angular
     .module('blocChat')
