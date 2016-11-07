9d11a8ae93bc214ce691ce16b62a24f519e9ac88
Praveen Yallakara <praveenyr@gmail.com>
2016-11-06 16:23:59 -0800

    Complete user story  - List chat messages for each room

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,15 +1,14 @@
 (function() {
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
-
     var rooms = $firebaseArray(ref);
 
-    window.foo = rooms;
+    window.foo = $firebaseArray(firebase.database().ref().child("messages"));
 
-    rooms.addRoom = function(name){;
-      console.log("Inside addRoom :" + name);
-        this.$add({name: name});
-    };
+    // rooms.addRoom = function(name){;
+    //   console.log("Inside addRoom :" + name);
+    //     this.$add({name: name});
+    // };
 
     return {
       all: rooms
--- dist/scripts/controllers/RoomCtrl.js
+++ dist/scripts/controllers/RoomCtrl.js
@@ -1,9 +1,17 @@
 (function() {
-    function RoomCollectionCtrl(Room)  {
+    function RoomCollectionCtrl(Room,Message)  {
         this.rooms = Room.all;
+        //Get messages by Room Id;
+        // this.messages = Message.getByRoomId(roomId);
+        this.setActiveRoom = (room) => {
+          this.activeRoom = room;
+          this.messages = Message.getByRoomId(room.$id);
+
+          console.log(this.messages);
+        }
       }
 
     angular
         .module('blocChat')
-        .controller('RoomCollectionCtrl',['Room', RoomCollectionCtrl] );
+        .controller('RoomCollectionCtrl',['Room','Message', RoomCollectionCtrl] );
 })();
