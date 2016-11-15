57d7653b0c2b7e7712b24d98a6225bf724121d60
Praveen Yallakara <praveenyr@gmail.com>
2016-11-14 21:17:50 -0800

    Complete Extra Credit - Add Firebase Authentication and Create Admin User

---
+++ dist/scripts/controllers/MyAuthCtrl.js
@@ -0,0 +1,55 @@
+(function() {
+    function MyAuthCtrl($firebaseAuth)  {
+      console.log("in auth ctrl");
+       this.myAuthObj = $firebaseAuth();
+       this.adminUser = false;
+
+
+       this.signInUser = () => {
+         //Prompt for userEmail
+         var userEmail;
+         while (!userEmail || userEmail === '') {
+          userEmail = prompt("Please enter your email: ");
+         }
+         //Prompt for user password
+            var userPwd;
+            while (!userPwd || userPwd === '') {
+              userPwd = prompt("Please enter your password: ");
+            }
+
+            //Sign in using userEmail and userPwd
+            this.myAuthObj.$signInWithEmailAndPassword(userEmail, userPwd)
+                .then(function(firebaseUser) {
+                console.log("Signed in as:", firebaseUser.uid);
+
+                }).catch(function(error) {
+                    console.error("Authentication failed:", error);
+                  });
+
+              var firebaseUser = this.myAuthObj.$getAuth();
+              //var firebaseUser =  firebase.auth().currentUser;
+            //  firebase.auth().onAuthStateChanged(function(firebaseUser) {
+                    console.log(firebaseUser);
+                    if (firebaseUser) {
+                      this.signedIn = true;
+                      if(firebaseUser.uid === '1slgmR0Alxfiy3szyq6Rl2Og5iK2'){
+                          this.adminUser = true;
+                        }
+                  }
+              //});
+
+          }
+
+        //Sign out existing signed in user
+        this.signOutUser = () => {
+          this.myAuthObj.$signOut();
+          this.signedIn = false;
+          this.adminUser = false;
+        }
+  }
+
+  angular
+    .module('blocChat')
+    .controller('MyAuthCtrl',["$firebaseAuth",MyAuthCtrl] );
+
+})();
--- dist/scripts/controllers/RoomCtrl.js
+++ dist/scripts/controllers/RoomCtrl.js
@@ -10,22 +10,25 @@
         this.setActiveRoom = (room) => {
           this.roomId = room.$id;
           this.activeRoom = room;
+          this.newMessage.roomId = this.activeRoom.$id;
           this.messages = Message.getByRoomId(this.roomId);
         };
 
           //Two way data binding when the user types in a message
-          this.chatmessage = ' ';
+        //  this.chatmessage = ' ';
+        this.newMessage = {
+          content:'',
+          roomId: this.roomId,
+          sentAt: "12:48 PM",
+          username: this.userName
+        };
 
           //Send a new chat message by invoking the Message Service.
           this.sendMessage = () => {
-            this.newMessage = {
-              content:this.chatmessage,
-              roomId: this.roomId,
-              sentAt: "12:48 PM",
-              username: this.userName
-            };
+            console.log(this.newMessage);
             console.log("In sendMessage");
             Message.send(this.newMessage);
+            this.newMessage.content = "";
         };
 
       }
